f9bf6840b2e41e3f10ac4c8e26e079b9d2a20616
lil-loki <lokesh.sivakumar@gmail.com>
2021-10-18 12:25:36 +0530

    small corrections

--- pg/src/main/java/com/lti/pg/controllers/LoanApplController.java
+++ pg/src/main/java/com/lti/pg/controllers/LoanApplController.java
@@ -2,7 +2,6 @@ package com.lti.pg.controllers;
 
 import java.util.List;
 
-import com.lti.pg.Dto.*;
 import com.lti.pg.beans.*;
 import com.lti.pg.controllers.LoanApplController.Status.StatusType;
 import com.lti.pg.exception.CustomerServiceException;
@@ -10,9 +9,7 @@ import com.lti.pg.services.ServiceLayer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
